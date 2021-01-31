76ed2869ec2996f72ddba35f753df42e11078f2a
Clinton Skakun <clinton.d.skakun@dedupe.ly>
2021-01-31 15:23:47 -0600

    Add cron checker

--- request.sh
+++ request.sh
@@ -4,6 +4,9 @@ method="$1"
 path="$2"
 conf="$3"
 key="$4"
+croncheckurl="$5"
 apiKey=$(ruby -ryaml -e "puts YAML.load_file('$conf')['$key']")
 
+curl -m 10 "$croncheckurl/start"
 curl -X $method "$path?apiKey=$apiKey" -vvv
+curl -m 10 "$croncheckurl/end"
